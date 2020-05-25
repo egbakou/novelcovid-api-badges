86e5a76edf7c17d393ecec75d781ae5cf5cf3b8b
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-25 23:31:59 +0000

    Added routes to the app

--- src/app.js
+++ src/app.js
@@ -8,8 +8,10 @@ const helmet = require("helmet");
 const NodeCache = require("node-cache");
 const md = require('markdown-it')();
 
-const app = express();
+const { httpErrorMiddleware } = require("./middleware");
+const routes = require("./routes");
 
+const app = express();
 const cache = new NodeCache({stdTTL: 100, checkperiod: 120});
 
 app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
@@ -17,21 +19,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use("/", routes);
+
 // render RENDER.md file as html for home page
 app.get("/", (_req, res) => {
-
     let readme = cache.get("readme");
-
     if (!readme) {
         readme = fs.readFileSync(path.join(__dirname, "../RENDER.md")).toString();
     }
-
     cache.set("readme", readme, 120);
-
     res.set('Content-Type', 'text/html;charset=utf-8')
-
     res.send(`<div>${md.render(readme)}</div>`)
-
 })
 
 
@@ -41,6 +39,6 @@ app.use((_req, _res, next) => {
 });
 
 // pass any errors to the error handler
-// app.use(httpErrorMiddleware);
+app.use(httpErrorMiddleware);
 
 module.exports = app;
